742f6e29aa13e76c24427c8f191e9a4e1c433d08
???? <4417452+rimnadze@users.noreply.github.com>
2013-11-09 01:13:22 +0400

    ??????

---
+++ დაფა.js
@@ -0,0 +1,17 @@
+exports.ჩაუშვი=function(პასუხ){
+    პასუხ.setHeader("Content-Type", "text/html; charset=utf-8")
+    პასუხ.end(
+        '<!DOCTYPE html>'+
+        '<html>'+
+            '<head>'+
+                '<link rel="stylesheet" href="/სახე.css">'+
+            '</head>'+
+            '<body>'+
+                '<div id="მთავარი_ნაჭერი">'+
+                '<canvas id="დაფა" onmousedown="თაგვზე_დაწკაპება()" onmouseup="თაგვის_აშვება()">'+
+                '</canvas>'+
+                '</div>'+
+                '<script type="text/javascript" src="/გულ-ღვიძლი.js"></script>'+
+            '</body>'+
+        '</html>')
+}
