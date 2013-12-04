8f6d678f57929365b5dbaba3c32d267b37850a63
???? <????@localhost>
2013-12-04 13:04:39 +0400

    ?????????????? ????????? ???????? ?? ???? ???????.

--- სახარახურე.js
+++ სახარახურე.js
@@ -7,3 +7,18 @@ var სახელი = document.getElementById('სახელი')
     }))
     მიე_ნამცხვარი('სახელი', სახელი.value)
 }
+
+var მომხმარებლები = document.getElementById('მომხმარებლები')
+გააგზავნე('/სად-არიან', function(უმი_ხალხი){
+    var ხალხი=JSON.parse(უმი_ხალხი)
+    console.log(ხალხი)
+    for(var მ in ხალხი){
+        var მომხმარებელი = ხალხი[მ],
+            ჩასამატებელი = document.createElement('option')
+        
+        ჩასამატებელი.appendChild(document.createTextNode(მომხმარებელი.სახელი))
+        ჩასამატებელი.value=მომხმარებელი.რიგის_ნომერი
+        
+        მომხმარებლები.appendChild(ჩასამატებელი)
+    }
+})
